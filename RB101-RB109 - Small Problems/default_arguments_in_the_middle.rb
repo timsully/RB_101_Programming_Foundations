7731054b894bd57e47a756c509dee6e77c4a753d
Tim Sullivan <timsullivan97@gmail.com>
2019-02-19 21:25:26 -0800

    complete default positional arguments exercise

--- RB101-RB109 - Small Problems/default_arguments_in_the_middle.rb
+++ RB101-RB109 - Small Problems/default_arguments_in_the_middle.rb
@@ -1,6 +1,8 @@
 =begin
 Consider the following method and a call to that method:
 =end
+# Using default positional arguments
+# Ruby will fill in the missing arguments in-order
 def my_method(a, b = 2, c = 3, d)
   p [a, b, c, d]
 end
