60589dfbcb054af0dba501c2c645503893f2c133
Tim Sullivan <timsullivan97@gmail.com>
2019-02-23 01:16:25 -0700

    fix typo

--- RB101-RB109 - Small Problems/keyword_arguments.rb
+++ RB101-RB109 - Small Problems/keyword_arguments.rb
@@ -7,6 +7,6 @@ classes Integer, Float, Numeric, and Math. Integer and Float
 inherit from Numeric. Numeric is basically the superclass of 
 all numeric types, while Math is a module that supplies a lot
 of number crunching functionality. As of Ruby 2.4.0 all integer
-valeus are Integer objects and no longer uses Ficnum and Bignum.
+values are Integer objects and no longer uses Ficnum and Bignum.
 =end
 5.step(by: 3, to: 10 ) { |value| puts value }
