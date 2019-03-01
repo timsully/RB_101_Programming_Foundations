553435b33d0f536ef962a0805d6383ed7a6e64f8
Tim Sullivan <timsullivan97@gmail.com>
2019-02-28 21:01:35 -0800

    add exercise problem

---
+++ RB101-RB109 - Small Problems/included_modules.rb
@@ -0,0 +1,8 @@
+=begin
+Use irb to run the following code:
+
+Find the documentation for the #min method and change the above code to
+print the two smallest values in the Array.
+=end
+a = [5, 9, 3, 11]
+puts a.min
