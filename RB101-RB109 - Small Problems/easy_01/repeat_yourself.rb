2c8282457a01b7d7591083e8c6c5e62a2d5d1927
Tim Sullivan <timsullivan97@gmail.com>
2019-03-01 18:44:10 -0800

    add folder and exercise

---
+++ RB101-RB109 - Small Problems/easy_01/repeat_yourself.rb
@@ -0,0 +1,10 @@
+=begin
+Write a method that takes two arguments, a string and a positive integer, and 
+prints as many times as the integer indicates.
+=end
+
+def repeat(string, int)
+  puts string * int
+end
+
+repeat('Hello', 3)
