5799683d65c766a862e6fe2657e85657c54265f1
Tim Sullivan <timsullivan97@gmail.com>
2019-03-01 18:33:24 -0800

    add link to Psych class

--- RB101-RB109 - Small Problems/down_the_rabbit_hole.rb
+++ RB101-RB109 - Small Problems/down_the_rabbit_hole.rb
@@ -6,6 +6,9 @@ a program that loads some text messages from a YAML file. We do this with
 YAML::load_file.
 
 Find the documentation for YAML::load_file.
+
+Link:
+http://ruby-doc.org/stdlib/libdoc/psych/rdoc/Psych.html#method-c-load_file
 =end
 require 'yaml'
 MESSAGES = YAML.load_file('calculator_messages.yml')
