b29874ab9aefe1e577e78ab66001873057717d91
Tim Sullivan <timsullivan97@gmail.com>
2019-03-01 19:01:56 -0800

    add solution to exercise

--- RB101-RB109 - Small Problems/easy_01/odd.rb
+++ RB101-RB109 - Small Problems/easy_01/odd.rb
@@ -6,14 +6,20 @@ odd. You may assume that the argument is a valid integer value.
 Keep in mind that you're not allowed to use #odd? or #even? in your solution.
 =end
 
-
+def to_be_or_not_to_be(lul)
+  if lul % 2 == 0
+    return false
+  else
+    return true
+  end
+end
 
 
 
 # Examples
-puts is_odd?(2)    # => false
-puts is_odd?(5)    # => true
-puts is_odd?(-17)  # => true
-puts is_odd?(-8)   # => false
-puts is_odd?(0)    # => false
-puts is_odd?(7)    # => true
+puts to_be_or_not_to_be(2)    # => false
+puts to_be_or_not_to_be(5)    # => true
+puts to_be_or_not_to_be(-17)  # => true
+puts to_be_or_not_to_be(-8)   # => false
+puts to_be_or_not_to_be(0)    # => false
+puts to_be_or_not_to_be(7)    # => true
