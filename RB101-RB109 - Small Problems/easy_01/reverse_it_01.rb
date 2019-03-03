6c8c89ebca217e281d76968487e453e8a5e54f33
Tim Sullivan <timsullivan97@gmail.com>
2019-03-02 19:33:05 -0800

    add exercise problem

---
+++ RB101-RB109 - Small Problems/easy_01/reverse_it_01.rb
@@ -0,0 +1,16 @@
+=begin
+Write a method that takes one argument, a string, and returns a new string with
+the words in reverse order.
+=end
+
+# method takes one argument
+def reverse_sentence(word)
+  
+
+
+end
+
+
+puts reverse_sentence('') == '' #=> true
+puts reverse_sentence('Hello World') == 'World Hello' #=> true
+puts reverse_sentence('Reverse these words') == 'words these Reverse' #=> true
