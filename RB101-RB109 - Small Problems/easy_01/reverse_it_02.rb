d7ac937b53f2393e176ea8af503fdc5a30483067
Tim Sullivan <timsullivan97@gmail.com>
2019-03-03 17:24:23 -0800

    add exercise problem

---
+++ RB101-RB109 - Small Problems/easy_01/reverse_it_02.rb
@@ -0,0 +1,12 @@
+=begin
+Write a method that takes one argument, a string containing one or more words,
+and returns the given string with all five or more letter words reversed. Each
+string will consist of only letters and spaces. Spaces should be included when 
+more than one word is present.
+=end
+
+
+# Test Cases
+puts reverse_words('Professional')          # => lanoisseforP
+puts reverse_words('Walk around the block') # => Walk dnuora the kcolb
+puts reverse_words('Launch School')         # => hcnuaL loohcS
