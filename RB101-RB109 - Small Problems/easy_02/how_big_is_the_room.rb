b1f9fb9569c4ebb13f23dd2e858565d531cb5ef9
Tim Sullivan <timsullivan97@gmail.com>
2019-03-10 21:26:42 -0700

    add exercise file

---
+++ RB101-RB109 - Small Problems/easy_02/how_big_is_the_room.rb
@@ -0,0 +1,52 @@
+=begin
+Build a program that asks a user for the length and the width of a room in
+meters and then displays the area of the room in both square meters and
+square feet. Don't worry about validating the input at this time.
+
+Note: 1 square meter == 10.7639 square feet
+
+PEDAC, Breaking down the Problem:
+
+Inputs:
+  - Program asks user for length and width of their room in meters
+
+Outputs:
+  - Displays the area of the room in both square feet and meters
+
+  rules:
+    - 
+
+Clarifying Questions?
+  - How do I figure out the conversion from meters to square feet?
+
+Mental Model:
+
+Ask for the length and then the width of the users room. Then calculate users input
+for both the length and width and turn it into meters and then also convert it into
+square feet.
+
+
+
+Examples/Test Cases
+
+------------------------
+Example 1
+
+Input
+  - 
+
+Output
+  - return what?
+------------------------
+
+=end
+
+
+=begin
+Examples:
+Enter the length of the room in meters:
+10
+Enter the width of the room in meters:
+7
+The area of the room is 70.0 square meters (753.47 square feet).
+=end 
