710ce68f86031cc9589fb7242a3b7159efe9b721
Tim Sullivan <timsullivan97@gmail.com>
2019-03-10 20:38:19 -0700

    add exercise folder and new exercise

---
+++ RB101-RB109 - Small Problems/easy_02/how_old_is_teddy.rb
@@ -0,0 +1,34 @@
+=begin
+Build a program that randomly generates and prints Teddy's age. To get the age, 
+you should generate a random number between 20 and 200.
+
+PEDAC, Breaking down the Problem:
+
+Inputs:
+  - nothing to input, program runs a method to generate random number
+
+Outputs:
+  - Output a string that that states how old teddy is between 20..100
+    "Teddy is x years old!"
+
+Implicit Rules:
+  - 
+
+Clarifying Questions?
+  - Will I be creating a method to call with no argument to trigger this program?
+
+Mental Model:
+
+
+
+Examples/Test Cases
+------------------------
+Example Output
+
+Output
+ Teddy is 69 years old!
+------------------------
+=end
+
+# Generate a random number between 20..200
+puts "Teddy is #{} years old!"
