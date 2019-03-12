dd02fb3b0ff88fa81c4bd520e11680c49f54f538
Tim Sullivan <timsullivan97@gmail.com>
2019-03-11 20:41:47 -0700

    add while loop approach

--- RB101-RB109 - Small Problems/easy_02/even_numbers.rb
+++ RB101-RB109 - Small Problems/easy_02/even_numbers.rb
@@ -2,4 +2,10 @@
 Print the even numbers from 1 to 99, inclusive. All numbers should be printed on
 separated lines.
 =end
-1.upto(99) { |i| puts i if i.even? }
+value = 1
+while value <= 99
+  puts value if value.even?
+  value += 1
+end
+
+# 1.upto(99) { |i| puts i if i.even? }
