1a8bbfd2c0ab2c20c33023ee56c5ba6425e23442
Tim Sullivan <timsullivan97@gmail.com>
2019-03-11 20:34:23 -0700

    add count odd numbers inclusively exercise

---
+++ RB101-RB109 - Small Problems/easy_02/odd_numbers.rb
@@ -0,0 +1,48 @@
+=begin
+Print all odd numbers 1 to 99, inclusive. All numbers should be printed
+on separate lines.
+
+PEDAC, Breaking down the Problem:
+
+Inputs:
+  - 
+
+Outputs:
+  - 
+
+  rules:
+    - 
+
+Implicit Rules:
+  - 
+
+  Integer:
+
+
+Clarifying Questions?
+  - 
+
+
+Mental Model:
+
+
+
+
+
+Examples/Test Cases
+------------------------
+Example 1
+
+Input
+  - an inclusive array of [1..99]
+
+Output
+  - Return all odd numbers from 1-99, each on their own line.
+------------------------
+=end
+value = 1
+
+while value <= 99
+  puts value
+  value += 2
+end
