11dec3cabadc910647020c09ea5a70278836976f
Tim Sullivan <timsullivan97@gmail.com>
2019-04-09 20:05:36 -0700

    add updates to calculator

--- lesson_02/mortgage_car_loan_calculator.rb
+++ lesson_02/mortgage_car_loan_calculator.rb
@@ -66,60 +66,60 @@ loop do
   prompt("Welcome to Mortgage Calculator")
   prompt("------------------------------")
 
-    prompt("What is the loan amount?")
+  prompt("What is the loan amount?")
     
-    # initialize variable explicitly
-    amount = ''
-    loop do
-      amount = Kernel.gets.chomp
+  # initialize variable explicitly
+  amount = ''
+  loop do
+    amount = Kernel.gets.chomp
 
-      if amount.empty? || amount.to_f < 0
-        prompt("Must enter positive number.")
-      else
-        break
-      end
+    if amount.empty? || amount.to_f < 0
+      prompt("Must enter positive number.")
+    else
+      break
     end
+  end
 
-    # Presenting an example to the user
-    prompt("What is the interest rate?")
-    prompt("(Example: 5 for 5% or 2.5 for 2.5%)")
+  # Presenting an example to the user
+  prompt("What is the interest rate?")
+  prompt("(Example: 5 for 5% or 2.5 for 2.5%)")
 
-    interest_rate = ''
-    loop do
-      interest_rate = Kernel.gets.chomp
+  interest_rate = ''
+  loop do
+    interest_rate = Kernel.gets.chomp
 
-      if interest_rate.empty? || interest_rate.to_f < 0
-        prompt("Must enter a positive number.")
-      else
-        break
-      end
+    if interest_rate.empty? || interest_rate.to_f < 0
+      prompt("Must enter a positive number.")
+    else
+      break
     end
+  end
 
-    prompt("What is the loan duration (in years)?")
+  prompt("What is the loan duration (in years)?")
 
-    years = ''
-    loop do
-      years = Kernel.gets.chomp
+  years = ''
+  loop do
+    years = Kernel.gets.chomp
 
-      if years.empty? || years.to_i < 0
-        prompt("Enter a valid number")
-      else
-        break
-      end
+    if years.empty? || years.to_i < 0
+      prompt("Enter a valid number")
+    else
+      break
     end
+  end
 
-    annual_interest_rate = interest_rate.to_f / 100
-    monthly_interest_rate = annual_interest_rate / 12
-    months = years.to_i * 12
+  annual_interest_rate = interest_rate.to_f / 100
+  monthly_interest_rate = annual_interest_rate / 12
+  months = years.to_i * 12
 
-    monthly_payment = amount.to_f * (monthly_interest_rate / (1 - (1 + monthly_interest_rate)**(-months)))
+  monthly_payment = amount.to_f * (monthly_interest_rate / (1 - (1 + monthly_interest_rate)**(-months)))
 
-    prompt("Your monthly payment is: $#{format('%02.2f', monthly_payment)}")
+  prompt("Your monthly payment is: $#{format('%02.2f', monthly_payment)}")
 
-    prompt("Another calculation?")
-    answer = Kernel.gets.chomp
+  prompt("Another calculation?")
+  answer = Kernel.gets.chomp
 
-    break unless answer.downcase.start_with?('y')
+  break unless answer.downcase.start_with?('y')
 end
 
 prompt("Thank you for using the Mortgage Calculator!")
