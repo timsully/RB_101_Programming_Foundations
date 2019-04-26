c455c47bab7eed67db146d5744a16c76f618d830
Tim Sullivan <timsullivan97@gmail.com>
2019-04-25 18:18:29 -0700

    update logic of conditional statement to accomodate lizard and spock

--- lesson_02/rock_paper_scissors_bonus_features.rb
+++ lesson_02/rock_paper_scissors_bonus_features.rb
@@ -1,4 +1,4 @@
-VALID_CHOICES = %w(rock paper scissors lizard spock)
+VALID_CHOICES = %w(rock r paper p scissors lizard l spock)
 
 def prompt(message)
   Kernel.puts("=> #{message}")
@@ -11,17 +11,17 @@ end
 # end
 
 def display_results(player, computer)
-  if (player == 'rock' && computer == 'scissors' || computer == 'lizard') ||
-      (player == 'paper' && computer == 'rock' || computer == 'spock') ||
-      (player == 'scissors' && computer == 'paper' || computer == 'lizard') ||
-      (player == 'lizard' && computer == 'paper' || computer == 'spock') ||
-      (player == 'spock' && computer == 'scissors' || computer == 'rock')
+  if (player == 'rock' || player == 'r' && computer == 'scissors' || computer == 'lizard' || computer == 'l') ||
+      (player == 'paper' || player == 'p' && computer == 'rock' || computer == 'r' || computer == 'spock') ||
+      (player == 'scissors' && computer == 'paper' || computer == 'p' || computer == 'lizard' || computer == 'l') ||
+      (player == 'lizard' || player == 'l' && computer == 'paper' || computer == 'p' || computer == 'spock') ||
+      (player == 'spock' && computer == 'scissors' || computer == 'rock' || computer == 'r')
     prompt("You won!")
-  elsif (computer == 'rock' && player == 'scissors' || player == 'lizard') ||
-        (computer == 'paper' && player == 'rock' || player == 'spock') ||
-        (computer == 'scissors' && player == 'paper' || player == 'lizard') ||
-        (computer == 'lizard' && player == 'paper' || player == 'spock') ||
-        (computer == 'spock' && player == 'scissors' || player == 'rock')
+  elsif (computer == 'rock' || computer == 'r' && player == 'scissors' || player == 'lizard' || player == 'l') ||
+        (computer == 'paper' || computer == 'p' && player == 'rock' || player == 'r' || player == 'spock') ||
+        (computer == 'scissors' && player == 'paper' || player == 'p' || player == 'lizard' || player == 'l') ||
+        (computer == 'lizard' || computer == 'l' && player == 'paper' || player == 'p' || player == 'spock') ||
+        (computer == 'spock' && player == 'scissors' || player == 'rock' || player == 'r')
     prompt("Computer won!")
   else player == computer
     prompt("It's a tie!")
