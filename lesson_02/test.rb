500af147043e784fc456633d32f61147238b08b7
Tim Sullivan <timsullivan97@gmail.com>
2019-04-29 19:21:04 -0700

    add test rpsls game

--- lesson_02/test.rb
+++ lesson_02/test.rb
@@ -6,54 +6,89 @@ The winner is displayed
 Bonus:
 Add Lizard and Spock
 =end
-VALID_CHOICES = %w(rock paper scissors lizard spock)
+VALID_CHOICES = %w(rock paper scissors lizard spock).freeze
+
+RULES = {
+  rock: [:scissors, :lizard],
+  paper: [:rock, :spock],
+  scissors: [:paper, :lizard],
+  spock: [:rock, :scissors],
+  lizard: [:paper, :spock]
+}.freeze
 
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
+def win?(first, second)
+  RULES[first.to_sym].include?(second.to_sym)
+end
+
 def display_results(player, computer)
-  if (player == 'rock' && computer == 'scissors' || computer == 'lizard') ||
-      (player == 'paper' && computer == 'rock' || computer == 'spock') ||
-      (player == 'scissors' && computer == 'paper' || computer == 'lizard') ||
-      (player == 'lizard' && computer == 'paper' || computer == 'spock') ||
-      (player == 'spock' && computer == 'scissors' || computer == 'rock')
+  if win?(player, computer)
     prompt("You won!")
-  elsif (computer == 'rock' && player == 'scissors' || player == 'lizard') ||
-        (computer == 'paper' && player == 'rock' || player == 'spock') ||
-        (computer == 'scissors' && player == 'paper' || player == 'lizard') ||
-        (computer == 'lizard' && player == 'paper' || player == 'spock') ||
-        (computer == 'spock' && player == 'scissors' || player == 'rock')
+  elsif win?(computer, player)
     prompt("Computer won!")
   else
     prompt("It's a tie!")
   end
 end
 
-# must be initialized outside of loop as well so that when
-# we compare choice to computer_choice var it is in scope
-loop do
-  choice = ''
-  loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    choice = Kernel.gets().chomp()
-
-    if VALID_CHOICES.include?(choice)
-      break
-    else
-      prompt("That's not a valid choice.")
-    end
-  end
-
-  computer_choice = VALID_CHOICES.sample
-
-  prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
-
-  display_results(choice, computer_choice)
-
-  prompt("Do you want to play again?")
-  answer = Kernel.gets().chomp()
-  break unless answer.downcase().start_with?('y')
+def update_score(score, player, computer)
+  score << 'player' if win?(player, computer)
+  score << 'computer' if win?(computer, player)
 end
 
-prompt("Thank you for playing, good bye!")
+def display_score(score)
+  player_wins = score.count('player')
+  computer_wins = score.count('computer')
+  prompt("The score is player: #{player_wins}, computer: #{computer_wins}.")
+  prompt("Keep going to get to 5 wins!") unless game_over?(score)
+end
+
+def game_over?(score)
+  score.count('player') >= 5 || score.count('computer') >= 5
+end
+
+# main loop
+loop do
+  score = []
+
+  # best of 9 loop, what is that exactly?
+  loop do
+    choice = nil
+    # validation loop
+    loop do
+      prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+      choice = gets.chomp.downcase
+
+      # validates choice based on beginning chars
+      break if (
+        choice = VALID_CHOICES.find do |valid_choice|
+          range_end = valid_choice.start_with?('s') ? 1 : 0
+          valid_choice[0..range_end] == choice[0..range_end]
+      end)
+
+      prompt("That's not a valid choice")
+    end
+  
+    computer_choice = VALID_CHOICES.sample
+
+    prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
+
+    display_results(choice, computer_choice)
+
+    update_score(score, choice, computer_choice)
+
+    display_score(score)
+
+    break if game_over?(score)
+  end
+
+  prompt("Game over, good game!")
+  prompt("Would you like to play again?")
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
+
+prompt("Thanks for playing, good bye!")
