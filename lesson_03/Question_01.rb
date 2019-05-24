3ba132671f504bb70de0e8bfc5a82f7dce896db2
Tim Sullivan <timsullivan97@gmail.com>
2019-05-23 19:17:43 -0700

    add solution to question 1

---
+++ lesson_03/Question_01.rb
@@ -0,0 +1,10 @@
+# Question 1
+# What would you expect the code below to print out?
+
+numbers = [1, 2, 2, 3]
+
+# Returns a new Array object with unique elements, but doesn't
+modify the original object
+numbers.uniq
+
+puts numbers #=> 1, 2, 2, 3
