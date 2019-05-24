d801272023fdbf03f44235e7fd30a613677d7ed4
Tim Sullivan <timsullivan97@gmail.com>
2019-05-23 19:19:20 -0700

    add answas to practice probs breh

--- lesson_03/practice_problems_medium_01.rb
+++ lesson_03/practice_problems_medium_01.rb
@@ -149,6 +149,7 @@ In an earlier practice problem we saw that depending on a method to modify its a
 
 def tricky_method(a_string_param, an_array_param)
   a_string_param += "rutabaga"
+  # adds rutabaga to end of array
   an_array_param << "rutabaga"
 end
 
@@ -163,7 +164,35 @@ We learned that whether the above "coincidentally" does what we think we wanted 
 
 How can we refactor this practice problem to make the result easier to predict and easier for the next programmer to maintain?
 
-It seems as if the two operations going on within the method contradict each other 
+It seems as if the two operations going on within the method contradict each other as one mutates and adds another strings into the array. Whereas the other operation just adds rutabaga to the end of the string if you were to isolate it into its own method.
+
+Code Refactored
+
+def not_so_tricky_method(a_string_param, an_array_param)
+  a_string_param += "rutabaga"
+  an_array_param += ["rutabaga"]
+
+  return a_string_param, an_array_param
+end
+
+my_string = "pumpkins"
+my_array = ["pumpkins"]
+my_string, my_array = not_so_tricky_method(my_string, my_array)
+
+puts "My string looks like this now: #{my_string}"
+puts "My array looks like this now: #{my_array}"
+
+
+Question 7
+answer = 42
+
+def mess_with_it(some_number)
+  some_number += 8
+end
+
+new_answer = mess_with_it(answer)
+
+p answer - 8
 
 
 =end
