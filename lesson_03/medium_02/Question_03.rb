1e82be0f28e57902cb586d6ff09896892c9d2ea5
Tim Sullivan <timsullivan97@gmail.com>
2019-05-26 16:25:31 -0700

    add exercise 3

---
+++ lesson_03/medium_02/Question_03.rb
@@ -0,0 +1,22 @@
+=begin
+Question 3
+
+Let's call a method, and pass both a string and an array as parameters and see how even though they are treated in the same way by Ruby, the results can be different.
+
+Study the following code and state what will be displayed and why:
+=end
+def tricky_method(a_string_param, an_array_param)
+  a_string_param += "rutabaga"
+  an_array_param << "rutabaga"
+end
+
+my_string = "pumpkins"
+my_array = ["pumpkins"]
+tricky_method(my_string, my_array)
+
+puts "My string looks like this now: #{my_string}"
+puts "My array looks like this now: #{my_array}"
+
+# Parameters are treated in exactly the same way, but the results are different. In both cases, Ruby passed the parameter "by value", but unlike some other languages, the that gets passed is a reference to some object.
+
+# a_string_param and my_string no longer point to the same object.
