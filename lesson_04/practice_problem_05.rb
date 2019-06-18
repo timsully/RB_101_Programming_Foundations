9544fb5a8bc6ac3e83f8ad5f15642c5adbed7bfe
Tim Sullivan <timsullivan97@gmail.com>
2019-06-17 20:56:38 -0700

    add exercise 5

---
+++ lesson_04/practice_problem_05.rb
@@ -0,0 +1,7 @@
+# Practice Problem 5
+# What does shift do in the following code? How can we find out?
+hash = { a: 'ant', b: 'bear' }
+hash.shift
+
+# Shift in the following code destructively removes the first key-value pair from hash and returns it as the two-item array
+# => ["a", "ant"]
