0e1a66216ce4a2ebf6228a99d6d4be7e94931e89
Tim Sullivan <timsullivan97@gmail.com>
2019-06-17 21:15:16 -0700

    compelete exercise 8

---
+++ lesson_04/practice_problem_08.rb
@@ -0,0 +1,6 @@
+# Practice Problem 8
+# How does take work? Is it destructive? How can we find out?
+arr = [1, 2, 3, 4, 5]
+arr.take(2)
+
+# Array#take returns the first n elements from the array. It is not destructive as it returns a new_ary. Check ruby docs to find out, lul.
