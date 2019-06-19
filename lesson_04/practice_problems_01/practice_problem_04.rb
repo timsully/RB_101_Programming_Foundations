8ee22544a48b62f916ce97aad68d06c5053156df
Tim Sullivan <timsullivan97@gmail.com>
2019-06-18 19:23:24 -0700

    add comment to better understand logic of method

--- lesson_04/practice_problems_01/practice_problem_04.rb
+++ lesson_04/practice_problems_01/practice_problem_04.rb
@@ -1,6 +1,8 @@
 # Practice Problem 4
 # What is the return value of each_with_object in the following code? Why?
 ['ant', 'bear', 'cat'].each_with_object({}) do |value, hash|
+  # Retrieving the first element in value (which is the first letter)
+  # creating a new key/value pair in hash (mutation)
   hash[value[0]] = value
 end
 
