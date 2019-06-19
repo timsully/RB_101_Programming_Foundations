a6b216d7f507b39d1d86b233d027702c50b0ce0a
Tim Sullivan <timsullivan97@gmail.com>
2019-06-18 18:02:46 -0700

    update typo

--- lesson_04/practice_problem_01.rb
+++ lesson_04/practice_problem_01.rb
@@ -7,5 +7,5 @@ end
 
 =begin
 Answer:
-select performs selection based on the truthiness of the block's return value. In this case the return value will always be 'hi', which is a "truthy" value. Therefor select will return a new array containing all of the elements in the original array.
+select performs selection based on the truthiness of the block's return value. In this case the return value will always be 'hi', which is a "truthy" value. Therefore select will return a new array containing all of the elements in the original array.
 =end
