9fbd6598eddd63d7f3650fad521fad6816a4d6c0
Tim Sullivan <timsullivan97@gmail.com>
2019-06-19 19:24:03 -0700

    update

--- lesson_04/practice_problems_02/practice_problem_03.rb
+++ lesson_04/practice_problems_02/practice_problem_03.rb
@@ -2,4 +2,10 @@
 # In the age hash, throw out the really old people (age 100 or older).
 ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
 
+# Removes every key-value pair from ages hash for which block evaluates to false
 ages.keep_if { |_, age| age < 100 }
+
+# Deletes every key-value pair from hsh for which block evaluates to false.
+
+
+h.select {|k,v| k > "a"}  #=> {"b" => 200, "c" => 300}
