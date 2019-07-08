2c32f3e45af4e167f6dae35d022942d76267dfac
Tim Sullivan <timsullivan97@gmail.com>
2019-07-07 18:11:26 -0700

    complete exercise 10

---
+++ lesson_04/practice_problem_10.rb
@@ -0,0 +1,20 @@
+# Practice Problem 10
+# Given the munsters hash below. Modify the hash such that each member of the Munster family has an additional "age_group" key that has one of three values describing the age group the family member is in (kid, adult, or senior). Your solution should produce the hash below
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+munsters.each do |name, details|
+  case details ["age"]
+  when 0...18
+    details["age_group"] = "kid"
+  when 18...65
+    details["age_group"] = "adult"
+  else
+    details["age_group"] = "senior"
+  end
+end
