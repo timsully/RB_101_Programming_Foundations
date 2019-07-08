de0ed2011d70ab57559d50b32898733b875c484a
Tim Sullivan <timsullivan97@gmail.com>
2019-07-07 17:36:26 -0700

    add exercise 7

---
+++ lesson_04/practice_problem_07.rb
@@ -0,0 +1,10 @@
+# Practice Problem 7
+# Create a hash that expresses the frequency with which each letter occurs in this string:
+statement = "The Flintstones Rock"
+
+result = {}
+letters = ('A'..'Z').to_a + ('a'..'z').to_a
+letters.each do |letter|
+  letter_frequency = statement.scan(letter).count
+  result[letter] = letter_frequency if letter_frequency > 0
+end
